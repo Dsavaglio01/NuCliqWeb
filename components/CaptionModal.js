2c2d633cee8609225af92059f6cda7e6deedc3f9
Dsavaglio01 <dosavaglio741@gmail.com>
2025-06-28 14:07:10 -0400

    Finishing up post/captions UI

--- components/CaptionModal.js
+++ components/CaptionModal.js
@@ -8,11 +8,11 @@ function CaptionModal({profile, data}) {
     <>
         <p className='text-white text-2xl'>Media Post</p>
         <div className='divider'/>
-        <div style={{width: '90%', marginLeft: '5%', maxHeight: 100}}>
+        <div style={{width: '90%', marginLeft: '5%',}}>
             {data && data.length > 1 ?
                 <CarouselComponent itemPost={data}/> : data && data.length == 1 ?
                 <div className="px-5 pb-5">
-                    <img src={data[0].post} className="w-full rounded-md" />
+                    <img src={data[0].post} style={{height: 400}} className="w-full rounded-md" />
                 </div> : null
                 }
         </div>
